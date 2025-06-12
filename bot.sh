bd3bb06ba5242b1dadab0e6412b22145bb21f51b
iruizz <isaacvert7@gmail.com>
2025-06-11 22:27:15 -0500

    change script logic

--- bot.sh
+++ bot.sh
@@ -4,17 +4,7 @@
 #
 # > https://github.com/theshteves/commit-bot
 #
-# Deploy locally by adding the following line to your crontab:
-# 0 22 * * * /bin/bash /<full-path-to-your-folder>/code/commit-bot/bot.sh
-#
-# Edit your crontab in vim w/ the simple command:
-# crontab -e
-#
-# Deploying just on your computer is better than a server if you want
-# your commits to more realistically mirror your computer usage.
-#
-# ...c'mon, nobody commits EVERY day ;)
-#
+
 info="Commit: $(date)"
 echo "OS detected: $OSTYPE"
 
@@ -32,16 +22,40 @@ case "$OSTYPE" in
         ;;
 esac
 
-echo "$info" >> output.txt
-echo "$info"
-echo
+# Define a pattern for commits
+# Example: Commit 3 times on Mondays, 1 time on Tuesdays, etc.
+day_of_week=$(date +%u) # 1 = Monday, 7 = Sunday
 
-# Detect current branch (main, master, etc)
-branch=$(git rev-parse --abbrev-ref HEAD)
+case "$day_of_week" in
+    1) commit_count=3 ;; # Monday
+    2) commit_count=1 ;; # Tuesday
+    3) commit_count=2 ;; # Wednesday
+    4) commit_count=4 ;; # Thursday
+    5) commit_count=1 ;; # Friday
+    6) commit_count=0 ;; # Saturday
+    7) commit_count=2 ;; # Sunday
+    *) commit_count=0 ;; # Fallback
+esac
 
-# Ship it
-git add output.txt
-git commit -m "$info"
-git push origin "$branch"
+# Perform the commits based on the pattern
+for ((i = 1; i <= commit_count; i++)); do
+    # Add a delay between commits to spread them out (e.g., 1-2 hours)
+    delay=$((RANDOM % 7200 + 3600)) # 3600s = 1 hour, 7200s = 2 hours
+    sleep "$delay"
+
+    # Generate commit message
+    info="Commit: $(date)"
+    echo "$info" >> output.txt
+    echo "$info"
+    echo
+
+    # Detect current branch (main, master, etc)
+    branch=$(git rev-parse --abbrev-ref HEAD)
+
+    # Ship it
+    git add output.txt
+    git commit -m "$info"
+    git push origin "$branch"
+done
 
 cd -
